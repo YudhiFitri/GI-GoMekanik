302fe4c9b20e7e24b6058aca343d55f765e142be
Yudhi_Ie <yudhi_ie@globalindointimates.com>
2021-06-10 17:17:36 +0700

    commit at 10/06/2021

--- app/Config/App.php
+++ app/Config/App.php
@@ -25,8 +25,8 @@ class App extends BaseConfig
 	 */
 	// public $baseURL = 'http://localhost:8080/';
 	// public $baseURL = 'http://localhost/GI-GoMekanik/public/';
-	// public $baseURL = 'http://192.168.10.81/GI-GoMekanik/public/';
-	public $baseURL = 'http://192.168.10.81/GI-GoMekanik/';
+	public $baseURL = 'http://192.168.10.81/GI-GoMekanik/public/';
+	// public $baseURL = 'http://192.168.10.81/GI-GoMekanik/';
 	// public $baseURL = 'https://GI-GoMekanik.com/';
 
 	/**
